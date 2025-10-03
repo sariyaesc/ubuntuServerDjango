fd87cfa0f50ab5084f81629321baaae1e4d5d1d3
sariyaesc <escamillasara49@gmail.com>
2025-10-02 19:19:35 -0600

    fix urls on test_page

--- test_page/test_page/urls.py
+++ test_page/test_page/urls.py
@@ -1,12 +1,7 @@
 from django.contrib import admin
-from django.urls import path, include   
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('hello/', include("mi_app.urls")), 
-    path("index2/",include("mi_app.urls")),  
-    path("tasks/", include("mi_app.urls")),
-    path("tasks/add/", include("mi_app.urls")),
-    path("tasks/admin/",include("mi_app.urls"))
+    path('', include('mi_app.urls')),   
 ]
- 
