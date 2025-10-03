5c1865f5b5a2b0341b14b95c6b0d13926155812d
sariyaesc <escamillasara49@gmail.com>
2025-10-02 19:50:25 -0600

    fix urls on test_page

--- test_page/mi_app/urls.py
+++ test_page/mi_app/urls.py
@@ -11,8 +11,8 @@ urlpatterns = [
     path("sumar/", views.sumar, name='sumar'),
 
     # Tareas
-    path("tasks/", views.tasks_index, name="tasks_index"),        # Lista de tareas en memoria
-    path("tasks/add/", views.tasks_add, name="tasks_add"),        # Agregar tarea manual
+    path("tasks_index/", views.tasks_index, name="tasks_index"),        # Lista de tareas en memoria
+    path("tasks/", views.tasks_add, name="tasks_add"),        # Agregar tarea manual
     path("admin-tasks/", views.tasks_admin_list, name="tasks_admin_list"),  # Lista desde admin
     path("index2/", views.index2, name="index2"),                 # Menú de tareas
 
